eebaa4bbf202e5961b368f08449f31a7e75cb1db
shubhdevnsg01 <petrifiedmaniac@gmail.com>
2023-12-31 15:38:27 +0530

    used hash router to deploy my app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { HashRouter as Router, Route,Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import Dashboard from './Dashboard';
 import './App.css'
